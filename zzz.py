1eee0f82e63297423660a42382770aa7c8d10197
waseemqayd1-netizen <waseemqayd1@gmail.com>
2026-02-13 22:12:41 +0300

    Update zzz.py

--- zzz.py
+++ zzz.py
@@ -242,7 +242,7 @@ def shop():
 # =======================
 # RUN
 # =======================
-
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
     
